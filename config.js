a91055a05649baf455dda3d5aa69d39ea03debb7
unknown <vutheducvn@gmail.com>
2021-10-10 20:48:47 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Hello bạn cùng phòng nhé!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao bạn cứ đi nhầm đường
+    Lạc vào tim mình thế`,
+    btnIntro: '😁😁😁',
+    title: 'Phải chăng bạn đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng bạn đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích nè  <33',
+    btnNo: 'Không đâu bạn ơi!!! :3',
+    question:'Trên thế giới gần 8 tỉ người mà sao bạn lại chọn mình thế <3',
+    btnReply: 'Gửi luôn <3',
+    reply: 'Yêu thì yêu mà không yêu bắt phải yêu <33333333',
+    mess: 'Mình biết mà, me too!!!😘😘',
+    messDesc: '!!!.',
+    btnAccept: 'Okiiiii lun nè <3',
+    messLink: 'https://www.facebook.com/DucVu789/' 
+}
